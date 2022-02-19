1ee89df53f5875a7e00f437a5aa0b3754226a515
unknown <abeeskhan.b@factweavers.com>
2022-02-19 20:19:36 +0530

    Initial Commit

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='flex items-center justify-center pt-10'>
+      <h2 className='text-4xl font-semibold'>Vite with preact </h2>
+    </div>
+  );
+};
+
+export default App;
